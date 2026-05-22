920d9eb589b3751d462c12482a1bcb3ad73b27ff
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-05-22 16:44:30 +0200

    Delete Implementation/AccountManager.php

--- Implementation/AccountManager.php
+++
@@ -1,8 +0,0 @@
-<?php
-class AccountManager
-{
-    public function createAccount(User $admin, int $id, string $email, string $name, string $password, Role $r): bool{}
-    public function modifyAccount(User $admin, int $id, string $email, string $name, string $password, Role $r): bool{}
-    public function deleteAccount(User $admin, int $id): bool{}
-}
-?>

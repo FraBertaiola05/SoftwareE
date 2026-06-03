9bd53eb7c06511c9d7ff594ef379c5788599c307
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-03 03:43:10 +0200

    Refactor login logic and integrate FlightScheduleSystem

--- Implementation/index.php
+++ Implementation/index.php
@@ -2,6 +2,7 @@
 session_start();
 require "DatabaseInfo.php";
 require "Classes/User.php";
+require "Classes/FlightScheduleSystem.php";
 if(!isset($_COOKIE["timeout"]))
     setcookie("timeout", 0, time() + 300, "/");
 try {
@@ -45,6 +46,8 @@ if(isset($_COOKIE["timeout"])&&$_COOKIE["timeout"]==5){
                 break;
         }
         $user=new User($r["id"],$r["name"],$r["surname"],$r["email"],$r["password"],$role);
+        if(isset($r["company_id"])&&!is_null($r["company_id"]))
+            $user->setCompany($r["company_id"]);
         if($user->login($_POST["email"],$_POST["password"])){
             $_SESSION["user"]=serialize($user);
             $error=null;
@@ -72,7 +75,7 @@ if(isset($_SESSION["user"])){
             header('Location: AdminPage.php');
             break;
         case RoleEnum::AirlineCompanyManager:
-            header('Location: .php');
+            header('Location: AirlinePage.php');
             break;
         case RoleEnum::AirportAnalyst:
             header('Location: AnalystPage.php');
@@ -99,78 +102,12 @@ if(isset($_SESSION["user"])){
                 echo "<p>".$error."</p>";
         ?>
         <p>Departures</p>
-        <table border=1>
-            <tr>
-                <th>Plane</th>
-                <th>Goes To</th>
-                <th>Airport</th>
-                <th>Departure Time</th>
-                <th>Gate</th>
-                <th>Status</th>
-            </tr>
-            <?php
-                try {
-                $query="SELECT f.plane_id AS 'plane', b.nation AS 'nation', b.city AS 'city', b.name AS 'airportName', f.scheduled_time AS 'depTime', gates.gate_number AS 'gateNumber', fs.status AS 'flightStatus'
-                FROM flights AS f INNER JOIN airports AS a ON a.id=f.departure_airport_id
-                INNER JOIN airports AS b ON b.id=f.arrival_airport_id
-                INNER JOIN flight_status AS fs ON f.status_id=fs.id
-                LEFT JOIN gates ON f.id=gates.flight_id
-                WHERE f.scheduled_time > CURDATE() AND a.code='POV'
-                ORDER BY f.scheduled_time";
-                
-                $result = $conn->query($query);
-
-                while($row = $result->fetch()) {
-                    echo "<tr>";
-                    echo "<td>" . $row['plane'] . "</td>";
-                    echo "<td>" . $row['city'] . ", " . $row['nation'] . "</td>";
-                    echo "<td>" . $row['airportName'] . "</td>";
-                    echo "<td>" . $row['depTime'] . "</td>";
-                    if($row["gateNumber"]!=NULL)
-                        echo "<td>" . $row['gateNumber'] . "</td>";
-                    else
-                        echo "<td> - </td>";
-                    echo "<td>" . $row['flightStatus'] . "</td>";
-                    echo "</tr>";
-                }
-                } catch(PDOException $e) {
-                    echo "Error: " . $e->getMessage();
-                }
-            ?>
-        </table>
+        <?php
+            echo FlightScheduleSystem::getFlightHistoryTable(true);
+        ?>
         <p>Arrivals</p>
-        <table border=1>
-            <tr>
-                <th>Plane</th>
-                <th>From</th>
-                <th>Airport</th>
-                <th>Arrival Time</th>
-                <th>Status</th>
-            </tr>
-            <?php
-                try {
-                $query="SELECT f.plane_id AS 'plane', a.nation AS 'nation', a.city AS 'city', a.name AS 'airportName', f.scheduled_time AS 'depTime', fs.status AS 'flightStatus'
-                FROM flights AS f INNER JOIN airports AS a ON a.id=f.departure_airport_id
-                INNER JOIN airports AS b ON b.id=f.arrival_airport_id
-                INNER JOIN flight_status AS fs ON f.status_id=fs.id
-                WHERE f.scheduled_time > CURDATE() AND b.code='POV'
-                ORDER BY f.scheduled_time";
-                
-                $result = $conn->query($query);
-
-                while($row = $result->fetch()) {
-                    echo "<tr>";
-                    echo "<td>".$row['plane']."</td>";
-                    echo "<td>".$row['city'].", ".$row['nation']."</td>";
-                    echo "<td>".$row['airportName']."</td>";
-                    echo "<td>".$row['depTime']."</td>";
-                    echo "<td>".$row['flightStatus']."</td>";
-                    echo "</tr>";
-                }
-                } catch(PDOException $e) {
-                    echo "Error: ".$e->getMessage();
-                }
-            ?>
-        </table>
+        <?php
+            echo FlightScheduleSystem::getFlightHistoryTable(false);
+        ?>
     </body>
 </html>

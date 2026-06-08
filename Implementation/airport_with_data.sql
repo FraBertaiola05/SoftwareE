499c7b40cc31ef58ac93022c38b6726b0cdc5fc3
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-08 04:09:12 +0200

    Update SQL schema and data for airport database

--- Implementation/airport_with_data.sql
+++ Implementation/airport_with_data.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Giu 03, 2026 alle 02:16
+-- Creato il: Giu 08, 2026 alle 02:55
 -- Versione del server: 10.4.32-MariaDB
 -- Versione PHP: 8.2.12
 
@@ -20,6 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `airport`
 --
+CREATE DATABASE IF NOT EXISTS `airport` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `airport`;
 
 -- --------------------------------------------------------
 
@@ -96,11 +98,12 @@ CREATE TABLE `flights` (
 --
 
 INSERT INTO `flights` (`id`, `priority`, `scheduled_time`, `validation`, `plane_id`, `pilot_id`, `departure_airport_id`, `arrival_airport_id`, `status_id`, `modify_id`) VALUES
-(1, 1, '2026-07-02 09:00:00', 'CONFIRMED', 'RYR32KE', 2, 3, 1, 4, NULL),
+(1, 1, '2026-07-02 09:00:00', 'CONFIRMED', 'RYR32KE', 2, 3, 1, 5, NULL),
 (2, 1, '2026-07-02 14:20:00', 'CONFIRMED', 'RYR32KE', 2, 1, 3, 1, NULL),
 (3, 2, '2026-07-03 10:30:00', 'CONFIRMED', 'DAL222', 3, 1, 2, 3, NULL),
 (4, 2, '2026-07-03 21:00:00', 'CONFIRMED', 'DAL222', 3, 2, 1, 1, NULL),
-(5, NULL, '2026-06-06 05:52:00', 'NOT_ACCEPTED', 'RYR32KE', 2, 1, 4, 4, NULL);
+(16, NULL, '2026-06-17 18:02:00', 'DELETED', 'RYR32KE', 2, 1, 3, 3, NULL),
+(20, NULL, '2026-06-26 01:08:00', 'CONFIRMED', 'RYR32KE', 2, 1, 3, 1, NULL);
 
 -- --------------------------------------------------------
 
@@ -120,6 +123,7 @@ CREATE TABLE `flight_status` (
 INSERT INTO `flight_status` (`id`, `status`) VALUES
 (2, 'Boarding'),
 (6, 'Cancelled'),
+(7, 'Finished'),
 (4, 'InQueueLanding'),
 (3, 'InQueueTakeOff'),
 (5, 'Landed'),
@@ -162,15 +166,15 @@ INSERT INTO `gates` (`id`, `gate_number`, `flight_id`) VALUES
 CREATE TABLE `parking_spots` (
   `id` int(11) NOT NULL,
   `spot_number` varchar(8) NOT NULL,
-  `flight_id` int(11) DEFAULT NULL
+  `plane_id` varchar(8) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `parking_spots`
 --
 
-INSERT INTO `parking_spots` (`id`, `spot_number`, `flight_id`) VALUES
-(1, 'PA1', NULL),
+INSERT INTO `parking_spots` (`id`, `spot_number`, `plane_id`) VALUES
+(1, 'PA1', 'RYR32KE'),
 (2, 'PA2', NULL),
 (3, 'PA3', NULL),
 (4, 'PA4', NULL),
@@ -200,7 +204,6 @@ INSERT INTO `parking_spots` (`id`, `spot_number`, `flight_id`) VALUES
 CREATE TABLE `planes` (
   `plane_number` varchar(8) NOT NULL,
   `model` varchar(255) NOT NULL,
-  `status_id` int(11) NOT NULL,
   `company_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -208,30 +211,9 @@ CREATE TABLE `planes` (
 -- Dump dei dati per la tabella `planes`
 --
 
-INSERT INTO `planes` (`plane_number`, `model`, `status_id`, `company_id`) VALUES
-('DAL222', 'Airbus A330-941', 4, 3),
-('RYR32KE', 'Boeing 737 MAX 8-200', 4, 7);
-
--- --------------------------------------------------------
-
---
--- Struttura della tabella `plane_status`
---
-
-CREATE TABLE `plane_status` (
-  `id` int(11) NOT NULL,
-  `status` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dump dei dati per la tabella `plane_status`
---
-
-INSERT INTO `plane_status` (`id`, `status`) VALUES
-(4, 'InFlight'),
-(3, 'OnRunway'),
-(1, 'Parked'),
-(2, 'Taxiing');
+INSERT INTO `planes` (`plane_number`, `model`, `company_id`) VALUES
+('DAL222', 'Airbus A330-941', 3),
+('RYR32KE', 'Boeing 737 MAX 8-200', 7);
 
 -- --------------------------------------------------------
 
@@ -269,6 +251,15 @@ CREATE TABLE `runways` (
   `flight_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dump dei dati per la tabella `runways`
+--
+
+INSERT INTO `runways` (`id`, `runway_number`, `flight_id`) VALUES
+(1, 'A1', NULL),
+(2, 'A2', NULL),
+(3, 'B1', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -279,6 +270,14 @@ CREATE TABLE `taxiways` (
   `id` int(11) NOT NULL,
   `taxiway_number` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dump dei dati per la tabella `taxiways`
+--
+
+INSERT INTO `taxiways` (`id`, `taxiway_number`) VALUES
+(1, 'A1'),
+(2, 'A2');
 
 -- --------------------------------------------------------
 
@@ -305,19 +304,23 @@ CREATE TABLE `users` (
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
   `role_id` int(11) NOT NULL,
-  `company_id` int(11) DEFAULT NULL
+  `company_id` int(11) DEFAULT NULL,
+  `password_reset` tinyint(1) DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `users`
 --
 
-INSERT INTO `users` (`id`, `email`, `password`, `name`, `surname`, `role_id`, `company_id`) VALUES
-(1, 'admin@admin.com', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 'Albert', 'Robinson', 5, NULL),
-(2, 'pilot@pilot.com', 'a4317322c01ef6d39c2ec0d46cb5070d973c7ea8c4f744a80c17d8a7b318d8799410f0149bdeca412dcf1510df369b34da3067eb5d4e5b880142ce0013589aff', 'Lucas', 'Fly', 2, 7),
-(3, 'pilota@pilota.com', '1d575c9b088d664e83b9c9c04d38a107e6dd75edfed386b4357b1f3578bdd0e8ade0731b4a81a2153e9bf9095251ba4f2aa2bd8f18ff1266aa63886f48b2ff60', 'Luca', 'Volo', 2, 3),
-(14, 'analyst@analyst.com', 'cc731411d0924f9ac456d49df60e525cea0d7aad1a652577859d794bfc830276b668f78421a8ed3dbc66f83b0d8e69a49b4779f43f2a56750ea7e25c8718be87', 'ana', 'list', 7, NULL),
-(15, 'airline@airline.com', '20e335154f790d5ac446712b2511b83c7292efa88665d9968289c2cdd83c7bbe74f87174faf2340cd6368b76453d592591bddbb4a3d417f71069ca205f8c05a7', 'air', 'line', 6, 7);
+INSERT INTO `users` (`id`, `email`, `password`, `name`, `surname`, `role_id`, `company_id`, `password_reset`) VALUES
+(1, 'admin@admin.com', 'c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec', 'Albert', 'Robinson', 5, NULL, 0),
+(2, 'pilot@pilot.com', 'a4317322c01ef6d39c2ec0d46cb5070d973c7ea8c4f744a80c17d8a7b318d8799410f0149bdeca412dcf1510df369b34da3067eb5d4e5b880142ce0013589aff', 'Lucas', 'Fly', 2, 7, 0),
+(3, 'pilota@pilota.com', '1d575c9b088d664e83b9c9c04d38a107e6dd75edfed386b4357b1f3578bdd0e8ade0731b4a81a2153e9bf9095251ba4f2aa2bd8f18ff1266aa63886f48b2ff60', 'Luca', 'Volo', 2, 3, 0),
+(14, 'analyst@analyst.com', 'cc731411d0924f9ac456d49df60e525cea0d7aad1a652577859d794bfc830276b668f78421a8ed3dbc66f83b0d8e69a49b4779f43f2a56750ea7e25c8718be87', 'ana', 'list', 7, NULL, 0),
+(15, 'airline@airline.com', '20e335154f790d5ac446712b2511b83c7292efa88665d9968289c2cdd83c7bbe74f87174faf2340cd6368b76453d592591bddbb4a3d417f71069ca205f8c05a7', 'air', 'line', 6, 7, 0),
+(16, 'ground@crew.com', 'fff7beb75e9ed4080857ffdf74e074a7b8a63923ce81606bc914c3cf89838091e54082060a54d32a45e9e560ad4bbea85689ae44581556e6d5203dc6fa08df2e', 'ground', 'crew', 3, NULL, 0),
+(17, 'tower@controller.com', '00c031b136a903266a44540cb4846430bfd91c8673b40bdeca50c017e9220e4dda3b429da92fb572da3a52681628686b86853f7d5d75d230031b6769593e8351', 'tower', 'controller', 1, NULL, 0),
+(18, 'gate@agent.com', 'bc2a235f2e2cb06b3c9df4932fc60146dd0b4771943ae86a6612d222f3aaf8027b357383569245aabd8508b0b6804a2bb5663bbde49567d663d3c9c737983f0b', 'gate', 'agent', 4, NULL, 0);
 
 --
 -- Indici per le tabelle scaricate
@@ -369,22 +372,14 @@ ALTER TABLE `gates`
 --
 ALTER TABLE `parking_spots`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `flight_fk3` (`flight_id`);
+  ADD KEY `plane_fk2` (`plane_id`);
 
 --
 -- Indici per le tabelle `planes`
 --
 ALTER TABLE `planes`
   ADD PRIMARY KEY (`plane_number`),
-  ADD KEY `company_fk2` (`company_id`),
-  ADD KEY `status_fk` (`status_id`);
-
---
--- Indici per le tabelle `plane_status`
---
-ALTER TABLE `plane_status`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `status` (`status`);
+  ADD KEY `company_fk2` (`company_id`);
 
 --
 -- Indici per le tabelle `roles`
@@ -397,6 +392,7 @@ ALTER TABLE `roles`
 -- Indici per le tabelle `runways`
 --
 ALTER TABLE `runways`
+  ADD PRIMARY KEY (`id`),
   ADD KEY `flight_fk4` (`flight_id`);
 
 --
@@ -442,13 +438,13 @@ ALTER TABLE `companies`
 -- AUTO_INCREMENT per la tabella `flights`
 --
 ALTER TABLE `flights`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT per la tabella `flight_status`
 --
 ALTER TABLE `flight_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT per la tabella `gates`
@@ -463,22 +459,22 @@ ALTER TABLE `parking_spots`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
--- AUTO_INCREMENT per la tabella `plane_status`
---
-ALTER TABLE `plane_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
 -- AUTO_INCREMENT per la tabella `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
+-- AUTO_INCREMENT per la tabella `runways`
+--
+ALTER TABLE `runways`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
 -- AUTO_INCREMENT per la tabella `taxiways`
 --
 ALTER TABLE `taxiways`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `taxiway_flight`
@@ -490,7 +486,7 @@ ALTER TABLE `taxiway_flight`
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- Limiti per le tabelle scaricate
@@ -517,14 +513,13 @@ ALTER TABLE `gates`
 -- Limiti per la tabella `parking_spots`
 --
 ALTER TABLE `parking_spots`
-  ADD CONSTRAINT `flight_fk3` FOREIGN KEY (`flight_id`) REFERENCES `flights` (`id`);
+  ADD CONSTRAINT `plane_fk2` FOREIGN KEY (`plane_id`) REFERENCES `planes` (`plane_number`);
 
 --
 -- Limiti per la tabella `planes`
 --
 ALTER TABLE `planes`
-  ADD CONSTRAINT `company_fk2` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`),
-  ADD CONSTRAINT `status_fk` FOREIGN KEY (`status_id`) REFERENCES `plane_status` (`id`);
+  ADD CONSTRAINT `company_fk2` FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`);
 
 --
 -- Limiti per la tabella `runways`

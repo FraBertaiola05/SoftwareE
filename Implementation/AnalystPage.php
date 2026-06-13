06aaa3ba92f5d024b9ad98b388dbd4a973986a44
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-13 20:44:37 +0200

    Add comments

--- Implementation/AnalystPage.php
+++ Implementation/AnalystPage.php
@@ -1,15 +1,24 @@
 <?php
+//Import required files
 require "Classes/User.php";
 require "Controllers/FinanceSystem.php";
+
+//Start the session to handle user login status
 session_start();
 $user=null;
+
+//Check if the user is logged with the correct role for this page. If not, redirect it to the login page
 if(isset($_SESSION["user"])&&unserialize($_SESSION["user"])->getRole()==RoleEnum::AirportAnalyst){
+    //Obtain the User object of the logged user
     $user=unserialize($_SESSION["user"]);
+    //If the user is logged but it still has the same password as when the account was created, redirrect it to the page to change the password
     if($user->getChangePass())
         header('Location: ChangePassword.php');
 }else{
     header('Location: index.php');
 }
+
+//Check if the user entered two dates and if the interval is valid. If it is, calculate cost and revenue of the timespan and print it to the user screen
 if(isset($_POST["sDate"])&&!is_null($_POST["sDate"])&&isset($_POST["fDate"])&&!is_null($_POST["fDate"])&&$_POST["fDate"]>=$_POST["sDate"]){
     [$cost, $revenue, $total]=FinanceSystem::getFinancialOverview($_POST["sDate"],$_POST["fDate"]);
     $s="<table border=1><tr><th>Start Date</th><th>End Date</th><th>Cost</th><th>Revenue</th><th>Total</th></tr>".
@@ -22,6 +31,7 @@ if(isset($_POST["sDate"])&&!is_null($_POST["sDate"])&&isset($_POST["fDate"])&&!i
         <title></title>
     </head>
     <body>
+        <!--Form to select the starting and finishing date-->
         <form action="AnalystPage.php" method="POST">
             <label for="sDate">From: </label>
             <input type="date" id="sDate" name="sDate" required>
@@ -30,9 +40,11 @@ if(isset($_POST["sDate"])&&!is_null($_POST["sDate"])&&isset($_POST["fDate"])&&!i
             <input type='submit' value='Calculate'>
         </form>
         <?php
+            //Print the table with the cost and revenue of the selected timestamp
             if(isset($s))
                 echo $s;
         ?>
+        <!--Button to redirect to the logout page-->
         <button onclick="window.location.href='Logout.php'">Logout</button>
     </body>
 </html>

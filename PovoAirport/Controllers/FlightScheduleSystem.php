8ae40700d2201651de81918db5d0673ba7e76e8c
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-14 00:55:58 +0200

    Add comments

--- PovoAirport/Controllers/FlightScheduleSystem.php
+++ PovoAirport/Controllers/FlightScheduleSystem.php
@@ -1,7 +1,9 @@
 <?php
 class FlightScheduleSystem
 {
+    //Given new filght data, create a new flight that will became definitive after the Tower Controller accept the flight
     public static function requestAddFlight(string $datetime, string $plane, int $pilot, int $dAirport, int $aAirport, int $status): string{
+        //Import required file
         require 'DatabaseInfo.php';
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -9,6 +11,7 @@ class FlightScheduleSystem
         } catch(PDOException $e){
             return "Could not connect. ".$e->getMessage();
         }
+        //Check if the inserted data is not null and correct
         if(!is_null($datetime)&&$datetime!=""&&strtotime($datetime)&&strtotime($datetime)>time()&&!is_null($plane)&&$plane!=""&&!is_null($pilot)&&!is_null($dAirport)&&!is_null($aAirport)&&!is_null($status)&&($dAirport==1||$aAirport==1)&&$dAirport!=$aAirport){
             try {
                 $query=$conn->prepare("INSERT INTO flights (scheduled_time, plane_id, pilot_id, departure_airport_id, arrival_airport_id, status_id) VALUES(:datetime,:plane,:pilot,:dAirport,:aAirport,:status)");
@@ -28,7 +31,9 @@ class FlightScheduleSystem
         }
     }
 
+    //Given modified filght data, create a new flight entry that modifies another one when the Tower Controller accept the changes
     public static function requestModifyFlight(string $datetime, string $plane, int $pilot, int $dAirport, int $aAirport, int $status, int $id): string{
+        //Import required file
         require 'DatabaseInfo.php';
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -36,6 +41,7 @@ class FlightScheduleSystem
         } catch(PDOException $e){
             return "Could not connect. ".$e->getMessage();
         }
+        //Check if the inserted data is not null and correct
         if(!is_null($datetime)&&$datetime!=""&&strtotime($datetime)&&strtotime($datetime)>time()&&!is_null($plane)&&$plane!=""&&!is_null($pilot)&&!is_null($dAirport)&&!is_null($aAirport)&&!is_null($status)&&($dAirport==1||$aAirport==1)&&$dAirport!=$aAirport){
             try {
                 $query=$conn->prepare("UPDATE flights SET scheduled_time=:datetime, plane_id=:plane, pilot_id=:pilot, departure_airport_id=:dAirport, arrival_airport_id=:aAirport, status_id=:status, validation='NOT_ACCEPTED' WHERE id=:id");
@@ -56,7 +62,9 @@ class FlightScheduleSystem
         }
     }
 
+    //Set a flight validation and status to "deleted"
     public static function deleteFlight(int $id): string{
+        //Import required file
         require 'DatabaseInfo.php';
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -78,7 +86,9 @@ class FlightScheduleSystem
         }
     }
 
+    //Update a flight entry that was accepted by the Tower Controller
     public static function updateAccepted(int $id): string{
+        //Import required file
         require 'DatabaseInfo.php';
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -100,7 +110,9 @@ class FlightScheduleSystem
         }
     }
 
+    //Delete a flight entry that was rejected by the Tower Controller
     public static function deleteRejected(int $id): string{
+        //Import required file
         require 'DatabaseInfo.php';
         try {
             $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -121,7 +133,9 @@ class FlightScheduleSystem
             return "The inserted data is wrong";
         }
     }
-
+    
+    //Get a table with the flight history. If $b=true, return the departures, while if $b=false, return the arrivals
+    //The function also take a starting and a finishing date as input. By default it goes from the moment you call the function onwards
     public static function getFlightHistoryTable($b=true, string $t1="", string $t2="9999-12-31 23:59:59.999"): string{
         $s="";
         if($t1==""){
@@ -129,6 +143,7 @@ class FlightScheduleSystem
             $t1=date_format($temp, 'd/m/Y H:i:s');
         }
         try {
+            //Import required file
             require 'DatabaseInfo.php';
             try {
                 $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
@@ -136,6 +151,7 @@ class FlightScheduleSystem
             } catch(PDOException $e){
                 return "Could not connect. ".$e->getMessage();
             }
+            //Fetch the departures and creates the table
             if($b){
                 $query="SELECT f.plane_id AS 'plane', b.nation AS 'nation', b.city AS 'city', b.name AS 'airportName', f.scheduled_time AS 'depTime', gates.gate_number AS 'gateNumber', fs.status AS 'flightStatus'
                 FROM flights AS f INNER JOIN airports AS a ON a.id=f.departure_airport_id
@@ -153,7 +169,7 @@ class FlightScheduleSystem
                         <th>Gate</th>
                         <th>Status</th>
                     </tr>";
-            }else{
+            }else{ //Fetch the arrivals and creates the table
                 $query="SELECT f.plane_id AS 'plane', a.nation AS 'nation', a.city AS 'city', a.name AS 'airportName', f.scheduled_time AS 'depTime', gates.gate_number AS 'gateNumber', fs.status AS 'flightStatus'
                 FROM flights AS f INNER JOIN airports AS a ON a.id=f.departure_airport_id
                 INNER JOIN airports AS b ON b.id=f.arrival_airport_id
@@ -174,6 +190,7 @@ class FlightScheduleSystem
             $result->bindParam(':t1',$t1);
             $result->bindParam(':t2',$t2);
             $result->execute();
+            //Insert the fetched data inside the table
             while($row = $result->fetch()) {
                 $s=$s."<tr>";
                 $s=$s."<td>" . $row['plane'] . "</td>";

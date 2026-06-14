5c2803a4e14556e4221b875a17564f729357d9c1
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-14 02:24:03 +0200

    Refactor Runway class to include Flight dependency

--- PovoAirport/Classes/Runway.php
+++ PovoAirport/Classes/Runway.php
@@ -1,8 +1,34 @@
 <?php
-require "Location.php";
-class Runway extends Location
+require "Flight.php";
+class Runway
 {
-    public int $runwayId;
+    public int $id;
+    public string $runwayNumber;
+    public Flight $flight;
 
+    public function __construct(int $id, string $runwayNumber, Flight $flight){
+        $this->id = $id;
+        $this->runwayNumber = $runwayNumber;
+        $this->flight = $flight;
+    }
+
+    public function getId(): int{
+        return $this->id;
+    }
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+    public function getRunwayNumber(): string{
+        return $this->runwayNumber;
+    }
+    public function setRunwayNumber(string $runwayNumber): void{
+        $this->runwayNumber = $runwayNumber;
+    }
+    public function getFlight(): Flight{
+        return $this->flight;
+    }
+    public function setFlight(Flight $flight): void{
+        $this->flight = $flight;
+    }
 }
 ?>

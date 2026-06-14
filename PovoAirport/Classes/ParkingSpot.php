129f30ef5703e3aedd1d620579b7bfa5571de0cc
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-14 02:23:28 +0200

    Refactor ParkingSpot class to include Plane dependency

--- PovoAirport/Classes/ParkingSpot.php
+++ PovoAirport/Classes/ParkingSpot.php
@@ -1,8 +1,34 @@
 <?php
-require "Location.php";
-class ParkingSpot extends Location
+require "Plane.php";
+class ParkingSpot
 {
-    public int $spotId;
+    public int $id;
+    public string $spotNumber;
+    public Plane $plane;
 
+    public function __construct(int $id, string $spotNumber, Plane $plane){
+        $this->id = $id;
+        $this->spotNumber = $spotNumber;
+        $this->plane = $plane;
+    }
+
+    public function getId(): int{
+        return $this->id;
+    }
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+    public function getSpotNumber(): string{
+        return $this->spotNumber;
+    }
+    public function setSpotNumber(string $spotNumber): void{
+        $this->spotNumber = $spotNumber;
+    }
+    public function getPlane(): Plane{
+        return $this->plane;
+    }
+    public function setPlane(Plane $plane): void{
+        $this->plane = $plane;
+    }
 }
 ?>

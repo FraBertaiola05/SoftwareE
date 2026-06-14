889207721b146dfcb3108c650654687a7ca5684b
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-14 02:21:24 +0200

    Add files via upload

---
+++ PovoAirport/Classes/TaxiwayFlight.php
@@ -0,0 +1,35 @@
+<?php
+require "Flight.php";
+require "Taxiway.php";
+class TaxiwayFlight
+{
+    private int $id;
+    private Flight $flight;
+    private Taxiway $taxiway;
+
+    public function __construct(int $id, Flight $flight, Taxiway $taxiway){
+        $this->id = $id;
+        $this->flight = $flight;
+        $this->taxiway = $taxiway;
+    }
+
+    public function getId(): int{
+        return $this->id;
+    }
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+    public function getFlight(): Flight{
+        return $this->flight;
+    }
+    public function setFlight(Flight $flight): void{
+        $this->flight = $flight;
+    }
+    public function getTaxiway(): Taxiway{
+        return $this->taxiway;
+    }
+    public function setTaxiway(Taxiway $taxiway): void{
+        $this->taxiway = $taxiway;
+    }
+}
+?>

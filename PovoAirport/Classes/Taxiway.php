878caa0370f7e363fa5cc56502397170d41923bb
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-14 02:24:16 +0200

    Refactor Taxiway class to remove inheritance and add properties

--- PovoAirport/Classes/Taxiway.php
+++ PovoAirport/Classes/Taxiway.php
@@ -1,8 +1,26 @@
 <?php
-require "Location.php";
-class Taxiway extends Location
-{
-    public string $code;
 
+class Taxiway
+{
+    public int $id;
+    public string $taxiwayNumber;
+
+    public function __construct(int $id, string $taxiwayNumber){
+        $this->id = $id;
+        $this->taxiwayNumber = $taxiwayNumber;
+    }
+
+    public function getId(): int{
+        return $this->id;
+    }
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+    public function getTaxiwayNumber(): string{
+        return $this->taxiwayNumber;
+    }
+    public function setTaxiwayNumber(string $taxiwayNumber): void{
+        $this->taxiwayNumber = $taxiwayNumber;
+    }
 }
 ?>

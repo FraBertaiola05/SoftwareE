29434bea14f2eb615bc7de595fc1552f177ec747
FraSpe22 <99676018+FraSpe22@users.noreply.github.com>
2026-06-14 02:23:13 +0200

    Refactor Gate class to include Flight dependency

--- PovoAirport/Classes/Gate.php
+++ PovoAirport/Classes/Gate.php
@@ -1,11 +1,34 @@
 <?php
-require "Location.php";
-class Gate extends Location
+require "Flight.php";
+class Gate
 {
-    public int $gateNumber;
-    public bool $isOccupied;
+    public int $id;
+    public string $gateNumber;
+    public Flight $flight;
 
-    public function assignFlight(Flight $f): void{}
-    public function freeGate(): bool{return $this->isOccupied;}
+    public function __construct(int $id, string $gateNumber, Flight $flight){
+        $this->id = $id;
+        $this->gateNumber = $gateNumber;
+        $this->flight = $flight;
+    }
+
+    public function getId(): int{
+        return $this->id;
+    }
+    public function setId(int $id): void{
+        $this->id = $id;
+    }
+    public function getGateNumber(): string{
+        return $this->gateNumber;
+    }
+    public function setGateNumber(string $gateNumber): void{
+        $this->gateNumber = $gateNumber;
+    }
+    public function getFlight(): Flight{
+        return $this->flight;
+    }
+    public function setFlight(Flight $flight): void{
+        $this->flight = $flight;
+    }
 }
 ?>
